5de06c6550463656afacc4b94fd381601922759e
Arjun <arjun346002@gmail.com>
2024-07-11 22:46:46 +0530

    redo

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,86 +1,12 @@
-// import { useState } from "react";
-
 import { useState } from "react";
 
-// const Form = () => {
-//   const [obj, setObj] = useState({
-//     name : "",
-//     age : "",
-//     city : ""
-//   });
-
-//   const getValue = (e) => {
-//     setObj({
-//       ...obj,
-//       [e.target.name]: e.target.value,
-//     });
-//   };
-
-//   const handleClick = (e) => {
-//     e.preventDefault();
-//     console.log(obj);
-//     setObj({
-//         name : "",
-//         age : "",
-//         city : ""
-//     });
-//   };
-
-//   return (
-//     <div>
-//       <form onSubmit={handleClick}>
-//         <input
-//           placeholder="Name.."
-//           name="name"
-//           type="text"
-//           value={obj.name}
-//           onChange={getValue}
-//         />
-//         <br />
-//         <br />
-//         <input
-//           placeholder="Age.."
-//           name="age"
-//           value={obj.age}
-//           onChange={getValue}
-//           type="number"
-//         />
-//         <br />
-//         <br />
-//         <input
-//           placeholder="City.."
-//           name="city"
-//           value={obj.city}
-//           onChange={getValue}
-//           type="text"
-//         />
-//         <br />
-//         <br />
-//         <button type="submit">Submit</button>
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default Form;
 
 const Form = () => {
   const [obj, setObj] = useState({
-    name: "",
-    age: null,
-    city: "",
+    name : "",
+    age : "",
+    city : ""
   });
-
-  const handleClick = (e) => {
-    e.preventDefault();
-
-    console.log(obj);
-    setObj({
-      name: "",
-      age: null,
-      city: "",
-    });
-  };
 
   const getValue = (e) => {
     setObj({
@@ -89,25 +15,53 @@ const Form = () => {
     });
   };
 
+  const handleClick = (e) => {
+    
+    e.preventDefault();
+    console.log(obj);
+    setObj({
+        name : "",
+        age : "",
+        city : ""
+    });
+  };
+
   return (
-    <div style={{ marginTop: "50px" }}>
+    <div>
       <form onSubmit={handleClick}>
-        <label>Name </label>
-        <input type="text" name="name" onChange={getValue} value={obj.name} />
+        <input
+          placeholder="Name.."
+          name="name"
+          type="text"
+          value={obj.name}
+          onChange={getValue}
+        />
         <br />
         <br />
-        <label>Age </label>
-        <input type="number" name="age" onChange={getValue} value={obj.age} />
+        <input
+          placeholder="Age.."
+          name="age"
+          value={obj.age}
+          onChange={getValue}
+          type="number"
+        />
         <br />
         <br />
-        <label>City </label>
-        <input type="text" name="city" onChange={getValue} value={obj.city} />
+        <input
+          placeholder="City.."
+          name="city"
+          value={obj.city}
+          onChange={getValue}
+          type="text"
+        />
         <br />
         <br />
-        <button>Submit</button>
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
 };
 
 export default Form;
+
+
